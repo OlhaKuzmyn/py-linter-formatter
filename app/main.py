4c4da1ea43cebfddf9d92af8b478234dfaf934f0
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-02-24 20:02:13 +0200

    fixed format_linter_report

--- app/main.py
+++ app/main.py
@@ -20,12 +20,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                format_linter_error(error) for error in errors
-            ],
-            "path": report,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(report, errors)
         for report, errors in linter_report.items()
     ]
